550f10c31f28dc0924b6d882612890b341a1018c
DasAleixo <contato.victorzmr@gmail.com>
2024-02-06 15:40:03 -0300

    responsive

--- src/components/PickCategory.jsx
+++ src/components/PickCategory.jsx
@@ -15,9 +15,9 @@ const PickCategory = () => {
     <div  className="flex flex-col">
         <h2 className="font-bold text-white text-3xl mb-5">Escolha uma categoria</h2>
         <p className="text-lg bg-clip-text bg-gradient-to-r from-violet-500 text-transparent mb-10">As perguntas serão referentes a uma das linguagens abaixo:</p>
-        <div className="flex justify-center items-center">
+        <div className="flex justify-center items-center gap-5">
             {quizState.questions.map((question) => (
-                <button onClick={() => chooseCategory(question.category)} key={question.category} className="border-2 border-white bg-gradient-to-r from-violet-500 text-white py-2 px-5 font-bold text-xl rounded-3xl shadow-xl mb-8 hover:from-violet-400 transition-colors mr-10">{question.category}</button>
+                <button onClick={() => chooseCategory(question.category)} key={question.category} className="border-2 border-white bg-gradient-to-r from-violet-500 text-white py-2 px-3 font-bold text-lg rounded-3xl shadow-xl mb-8 hover:from-violet-400 transition-colors sm:py-2 sm:px-5 sm:text-xl">{question.category}</button>
             ))}
         </div>
         <img src={category} alt="Categorias do Quiz" className="h-72"/>
